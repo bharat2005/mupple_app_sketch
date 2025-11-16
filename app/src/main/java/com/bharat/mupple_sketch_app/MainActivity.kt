627f86252105775b7775d558143f522a0fa5d230
bharat2005 <bharatvdeshm2005@gmail.com>
2025-11-16 14:52:37 +0530

    first commit

---
+++ app/src/main/java/com/bharat/mupple_sketch_app/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bharat.mupple_sketch_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bharat.mupple_sketch_app.ui.theme.Mupple_sketch_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Mupple_sketch_appTheme {
+
+            }
+        }
+    }
+}
+
